ddcce48756b154295b0f988558bcefaa3d4a2af9
wenhongliang <wenhong.liang@shopee.com>
2020-09-15 14:29:54 +0800

    java的时间轮

---
+++ Kafka/时间轮/MyTimeWheel/TimeTask.java
@@ -0,0 +1,38 @@
+
+/**
+ * @Author: wenhongliang
+ */
+public class TimeTask {
+    /**
+     * 延迟执行时间
+     */
+    private long delayMs;
+    /**
+     * 过期时间戳
+     */
+    private long expireTimestamp;
+
+    private Runnable task;
+
+    protected Bucket bucket;
+
+    protected TimeTask pre;
+    protected TimeTask next;
+
+    public TimeTask(long delayMs, Runnable task) {
+        this.delayMs = delayMs;
+        this.task = task;
+        this.bucket = null;
+        this.pre = null;
+        this.next = null;
+        this.expireTimestamp = System.currentTimeMillis() + delayMs;
+    }
+
+    public Runnable getTask() {
+        return task;
+    }
+
+    public long getExpireTimestamp() {
+        return expireTimestamp;
+    }
+}
