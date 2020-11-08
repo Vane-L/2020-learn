b36e7a20f35a34b6b2eff2adfa7bebe91499e84b
wenhongliang <wenhong.liang@shopee.com>
2020-11-08 21:13:50 +0800

    code

---
+++ code/src/com/liang/greddy/Easy.java
@@ -0,0 +1,46 @@
+package com.liang.greddy;
+
+
+/**
+ * @Author: wenhongliang
+ */
+public class Easy {
+    public static void main(String[] args) {
+        Easy easy = new Easy();
+        System.out.println(easy.maxProfit121(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(easy.maxProfit122(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    //输入: [7,1,5,3,6,4]
+    //输出: 7
+    //解释: 在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 这笔交易所能获得利润 = 5-1 = 4 。
+    //     随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 这笔交易所能获得利润 = 6-3 = 3 。
+    public int maxProfit122(int[] prices) {
+        int res = 0;
+        for (int i = 1; i < prices.length; i++) {
+            res += Math.max(0, prices[i] - prices[i - 1]);
+        }
+        return res;
+    }
+
+
+    //输入: [7,1,5,3,6,4]
+    //输出: 5
+    //解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+    //     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+    public int maxProfit121(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (maxProfit < prices[i] - minPrice) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ code/src/com/liang/tree/Hard.java
@@ -0,0 +1,24 @@
+package com.liang.tree;
+
+/**
+ * @Author: wenhongliang
+ */
+public class Hard {
+
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        getMaxSum(root);
+        return maxSum;
+    }
+
+    public int getMaxSum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(0, getMaxSum(root.left));
+        int right = Math.max(0, getMaxSum(root.right));
+        maxSum = Math.max(maxSum, root.val + left + right);
+        return root.val + Math.max(left, right);
+    }
+}
