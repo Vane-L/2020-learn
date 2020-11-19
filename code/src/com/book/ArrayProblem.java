cb91f3ec0a239999a97c858fe484a5be36b1b3c9
wenhongliang <wenhong.liang@shopee.com>
2020-11-19 16:06:36 +0800

    book

--- code/src/com/book/ArrayProblem.java
+++ code/src/com/book/ArrayProblem.java
@@ -6,12 +6,14 @@ package com.book;
 public class ArrayProblem {
     public static void main(String[] args) {
         ArrayProblem ap = new ArrayProblem();
-        ap.orderPrint(new int[][]{
+        int[][] matrix = new int[][]{
                 {1, 2, 3, 4},
                 {5, 6, 7, 8},
                 {9, 10, 11, 12},
                 {13, 14, 15, 16}
-        });
+        };
+        ap.orderPrint(matrix);
+        ap.rotate(matrix);
     }
 
     public void orderPrint(int[][] matrix) {
@@ -22,30 +24,49 @@ public class ArrayProblem {
         }
     }
 
-    private void printEdge(int[][] matrix, int tR, int tC, int dR, int dC) {
+    private void printEdge(int[][] m, int tR, int tC, int dR, int dC) {
         if (tR == dR) {
             for (int i = tC; i <= dC; i++) {
-                System.out.print(matrix[tR][i]);
+                System.out.print(m[tR][i]);
             }
         } else if (tC == dC) {
             for (int i = tR; i <= dR; i++) {
-                System.out.print(matrix[i][tC]);
+                System.out.print(m[i][tC]);
             }
         } else {
             int curR = tR;
             int curC = tC;
             while (curC != dC) {
-                System.out.print(matrix[curR][curC++] + " ");
+                System.out.print(m[curR][curC++] + " ");
             }
             while (curR != dR) {
-                System.out.print(matrix[curR++][curC] + " ");
+                System.out.print(m[curR++][curC] + " ");
             }
             while (curC != tC) {
-                System.out.print(matrix[curR][curC--] + " ");
+                System.out.print(m[curR][curC--] + " ");
             }
             while (curR != tR) {
-                System.out.print(matrix[curR--][curC] + " ");
+                System.out.print(m[curR--][curC] + " ");
             }
+        }
+    }
+
+    public void rotate(int[][] matrix) {
+        int tR = 0, tC = 0;
+        int dR = matrix.length - 1, dC = matrix[0].length - 1;
+        while (tR < dR) {
+            printRotate(matrix, tR++, tC++, dR--, dC--);
+        }
+    }
+
+    private void printRotate(int[][] m, int tR, int tC, int dR, int dC) {
+        int times = dC - tC;
+        for (int i = 0; i != times; i++) {
+            int tmp = m[tR][tC + i];
+            m[tR][tC + i] = m[dR-i][tC];
+            m[dR-i][tC] = m[dR][dC - i];
+            m[dR][dC - i] = m[tR + i][dC];
+            m[tR + i][dC] = tmp;
         }
     }
 }
