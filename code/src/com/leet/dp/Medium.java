117544a602f24882e7573df4efdde5ece96c04b8
wenhongliang <wenhong.liang@shopee.com>
2021-06-14 21:24:04 +0800

    code dp

--- code/src/com/leet/dp/Medium.java
+++ code/src/com/leet/dp/Medium.java
@@ -1,5 +1,7 @@
 package com.leet.dp;
 
+import java.util.Arrays;
+
 /**
  * @Author: wenhongliang
  */
@@ -71,5 +73,62 @@ public class Medium {
             right++;
         }
         return s.substring(left + 1, right);
+    }
+
+    public boolean canJump(int[] nums) {
+        int tmp = 0;
+        for (int i = 0; i <= tmp; i++) {
+            tmp = Math.max(tmp, i + nums[i]);
+            if (tmp >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    // [1,2,5] 11
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (i >= coin && dp[i - coin] != amount + 1) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+
+    public int lengthOfLIS(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        return max;
     }
 }
