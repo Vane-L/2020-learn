c3e2f49d39c33420d3e5d0e4f488b3f42dbefe8c
wenhongliang <wenhong.liang@shopee.com>
2021-06-26 21:58:09 +0800

    code at 0626

--- code/src/com/leet/backtrack/Medium.java
+++ code/src/com/leet/backtrack/Medium.java
@@ -1,6 +1,7 @@
 package com.leet.backtrack;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -116,6 +117,173 @@ public class Medium {
                 backWord(board, i, j + 1, word, idx + 1);
         board[i][j] = tmp;
         return flag;
+    }
+
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isEdge = i == 0 || j == 0 || i == m - 1 || j == n - 1;
+                if (isEdge && board[i][j] == 'O') {
+                    backX(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+                if (board[i][j] == '#') {
+                    board[i][j] = 'O';
+                }
+            }
+        }
+
+    }
+
+    private void backX(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] == 'X' || board[i][j] == '#') {
+            return;
+        }
+        board[i][j] = '#';
+        backX(board, i - 1, j);
+        backX(board, i + 1, j);
+        backX(board, i, j - 1);
+        backX(board, i, j + 1);
+    }
+
+
+    public int findCircleNum(int[][] isConnected) {
+        int provinces = isConnected.length;
+        // 记录城市是否访问过
+        boolean[] visited = new boolean[provinces];
+        int circles = 0;
+        for (int i = 0; i < provinces; i++) {
+            // 从未被访问过的城市开始DFS
+            if (!visited[i]) {
+                dfs(isConnected, visited, provinces, i);
+                circles++;
+            }
+        }
+        return circles;
+    }
+
+    public void dfs(int[][] isConnected, boolean[] visited, int provinces, int i) {
+        for (int j = 0; j < provinces; j++) {
+            if (isConnected[i][j] == 1 && !visited[j]) {
+                visited[j] = true;
+                dfs(isConnected, visited, provinces, j);
+            }
+        }
+    }
+
+    public int longestIncreasingPath(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, backIncr(matrix, i, j, -1, 0));
+            }
+        }
+        return max;
+    }
+
+    private int backIncr(int[][] matrix, int i, int j, int pre, int cur) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length || matrix[i][j] < 0 || matrix[i][j] <= pre) {
+            return cur;
+        }
+        cur++;
+        int tmp = matrix[i][j];
+        matrix[i][j] = -1;
+        int left = backIncr(matrix, i, j - 1, tmp, cur);
+        int right = backIncr(matrix, i, j + 1, tmp, cur);
+        int down = backIncr(matrix, i + 1, j, tmp, cur);
+        int up = backIncr(matrix, i - 1, j, tmp, cur);
+        matrix[i][j] = tmp;
+        return Math.max(left, Math.max(right, Math.max(down, up)));
+    }
+
+    public int longestIncreasingPath2(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) {
+            return 0;
+        }
+        int m = matrix.length, n = matrix[0].length;
+        int[][] used = new int[m][n];
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                max = Math.max(max, backIncr2(matrix, i, j, used));
+            }
+        }
+        return max;
+    }
+
+    private int backIncr2(int[][] matrix, int i, int j, int[][] used) {
+        if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) {
+            return 0;
+        }
+        if (used[i][j] > 0) {
+            return used[i][j];
+        }
+        int num = 0;
+        if (i - 1 >= 0 && matrix[i - 1][j] < matrix[i][j]) {
+            num = Math.max(num, backIncr2(matrix, i - 1, j, used));
+        }
+        if (i + 1 < matrix.length && matrix[i + 1][j] < matrix[i][j]) {
+            num = Math.max(num, backIncr2(matrix, i + 1, j, used));
+        }
+        if (j - 1 >= 0 && matrix[i][j - 1] < matrix[i][j]) {
+            num = Math.max(num, backIncr2(matrix, i, j - 1, used));
+        }
+        if (j + 1 < matrix[0].length && matrix[i][j + 1] < matrix[i][j]) {
+            num = Math.max(num, backIncr2(matrix, i, j + 1, used));
+        }
+        used[i][j] = num + 1;
+        return num + 1;
+    }
+
+    public List<Integer> countSmaller(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return list;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int count = 0;
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                }
+            }
+            list.add(count);
+        }
+        return list;
+    }
+
+    public List<Integer> countSmaller2(int[] nums) {
+        if (nums == null || nums.length == 0) return new LinkedList<>();
+        LinkedList<Integer> res = new LinkedList<>();
+        int len = nums.length;
+        //反向插入排序
+        for (int i = len - 2; i >= 0; i--) {
+            int j = i + 1, tmp = nums[i];
+            while (j < len && nums[j] >= tmp) {
+                nums[j - 1] = nums[j];
+                j++;
+            }
+            nums[j - 1] = tmp;
+            //len - j就表示计数个数
+            res.addFirst(len - j);
+        }
+        //添加最后一个数
+        res.add(0);
+        return res;
     }
 
 }
--- code/src/com/leet/tree/Hard.java
+++ code/src/com/leet/tree/Hard.java
@@ -1,5 +1,12 @@
 package com.leet.tree;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
 /**
  * @Author: wenhongliang
  */
@@ -20,5 +27,194 @@ public class Hard {
         int right = Math.max(0, getMaxSum(root.right));
         maxSum = Math.max(maxSum, root.val + left + right);
         return root.val + Math.max(left, right);
+    }
+
+    // 单词接龙
+    public int ladderLengthBFS1Q(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        boolean[] visited = new boolean[wordList.size()];
+        if (wordList.contains(beginWord)) {
+            visited[wordList.indexOf(beginWord)] = true;
+        }
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        int count = 1;
+        while (!queue.isEmpty()) {
+            count++;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String tmp = queue.poll();
+                for (int j = 0; j < wordList.size(); j++) {
+                    if (visited[j] || !canCovert(tmp, wordList.get(j))) {
+                        continue;
+                    }
+                    if (wordList.get(j).equals(endWord)) {
+                        return count;
+                    }
+                    visited[j] = true;
+                    queue.offer(wordList.get(j));
+                }
+            }
+        }
+        return 0;
+    }
+
+    public int ladderLengthBFS2Q(String beginWord, String endWord, List<String> wordList) {
+        if (!wordList.contains(endWord)) {
+            return 0;
+        }
+        Queue<String> queue1 = new LinkedList<>();
+        Queue<String> queue2 = new LinkedList<>();
+        queue1.offer(beginWord);
+        queue2.offer(endWord);
+        Set<String> visited1 = new HashSet<>();
+        Set<String> visited2 = new HashSet<>();
+        visited1.add(beginWord);
+        visited2.add(endWord);
+        int count1 = 0;
+        int count2 = 0;
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            count1++;
+            int size1 = queue1.size();
+            for (int i = 0; i < size1; i++) {
+                String tmp = queue1.poll();
+                for (int j = 0; j < wordList.size(); j++) {
+                    String word = wordList.get(j);
+                    if (visited1.contains(word) || !canCovert(tmp, word)) {
+                        continue;
+                    }
+                    if (visited2.contains(word)) {
+                        return count1 + count2 + 1;
+                    }
+                    visited1.add(word);
+                    queue1.offer(word);
+                }
+            }
+            count2++;
+            int size2 = queue2.size();
+            for (int i = 0; i < size2; i++) {
+                String tmp = queue2.poll();
+                for (int j = 0; j < wordList.size(); j++) {
+                    String word = wordList.get(j);
+                    if (visited2.contains(word) || !canCovert(tmp, word)) {
+                        continue;
+                    }
+                    if (visited1.contains(word)) {
+                        return count1 + count2 + 1;
+                    }
+                    visited2.add(word);
+                    queue2.offer(word);
+                }
+            }
+        }
+        return 0;
+    }
+
+    private boolean canCovert(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        int count = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                ++count;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        return count == 1;
+    }
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] degree = new int[numCourses];
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] cp : prerequisites) {
+            degree[cp[0]]++;
+            list.get(cp[1]).add(cp[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for (int cur : list.get(pre)) {
+                if (--degree[cur] == 0) {
+                    queue.offer(cur);
+                }
+            }
+        }
+        return numCourses == 0;
+    }
+
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        int idx = 0;
+        int[] res = new int[numCourses];
+        int[] degree = new int[numCourses];
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] cp : prerequisites) {
+            degree[cp[0]]++;
+            list.get(cp[1]).add(cp[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+                res[idx++] = i;
+            }
+        }
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            res[idx++] = pre;
+            for (int cur : list.get(pre)) {
+                if (--degree[cur] == 0) {
+                    queue.offer(cur);
+                }
+            }
+        }
+        return res;
+    }
+
+    public int[] findOrder2(int numCourses, int[][] prerequisites) {
+        int idx = 0;
+        int[] res = new int[numCourses];
+        int[] degree = new int[numCourses];
+        List<List<Integer>> list = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList<>());
+        }
+        for (int[] cp : prerequisites) {
+            degree[cp[0]]++;
+            list.get(cp[1]).add(cp[0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (degree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            res[idx++] = pre;
+            for (int cur : list.get(pre)) {
+                if (--degree[cur] == 0) {
+                    queue.offer(cur);
+                }
+            }
+        }
+        if (numCourses != 0) return new int[0];
+        return res;
     }
 }
