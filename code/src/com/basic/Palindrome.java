66ea5e3c75d640512e9ff050ec20cc3528598d56
wenhongliang <wenhong.liang@shopee.com>
2021-11-13 16:18:13 +0800

    递归解决回文串

--- code/src/com/basic/Palindrome.java
+++ code/src/com/basic/Palindrome.java
@@ -7,6 +7,12 @@ public class Palindrome {
     public static void main(String[] args) {
         System.out.println("noon is " + isPalindrome("noon"));
         System.out.println("moon is " + isPalindrome("moon"));
+
+        System.out.println("noon is " + isPalindromeD("noon"));
+        System.out.println("moon is " + isPalindromeD("moon"));
+
+        System.out.println("noon is " + isPalindromeD("noon", 0, 3));
+        System.out.println("moon is " + isPalindromeD("moon", 0, 3));
     }
 
     public static boolean isPalindrome(String s) {
@@ -20,4 +26,22 @@ public class Palindrome {
         }
         return true;
     }
+
+    public static boolean isPalindromeD(String s) {
+        if (s.length() <= 1) {
+            return true;
+        }
+        if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+        return isPalindrome(s.substring(1, s.length() - 1));
+    }
+
+    public static boolean isPalindromeD(String s, int low, int high) {
+        if (high <= low) return true;
+        if (s.charAt(low) != s.charAt(high)) return false;
+        return isPalindromeD(s, low + 1, high - 1);
+    }
+
+
 }
